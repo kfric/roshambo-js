0f12c254a0f625c42f1aade48e51bc6a4e566828
Karl Frick <karl.frick5@gmail.com>
2021-05-21 11:39:21 -0400

    Created function runGame to determine winner after each selection from player2

--- public/main.js
+++ public/main.js
@@ -1,7 +1,6 @@
 function main() {
   let p1 = 'p1'
   let p2 = 'p2'
-
   // handles if player1 selects rock
   const selectRockP1 = document.querySelector('.player1 .rock')
   function clickRockP1() {
@@ -10,7 +9,6 @@ function main() {
     p1 = 'ROCK'
   }
   selectRockP1.addEventListener('click', clickRockP1)
-
   // handles if player1 selects paper
   const selectPaperP1 = document.querySelector('.player1 .paper')
   function clickPaperP1() {
@@ -27,7 +25,6 @@ function main() {
     p1 = 'SCISSORS'
   }
   selectScissorsP1.addEventListener('click', clickScissorsP1)
-
   // player2--------------------------------------------------------
   // handles if player2 selects rock
   const selectRockP2 = document.querySelector('.player2 .rock')
@@ -35,6 +32,7 @@ function main() {
     const player2Choice = document.querySelector('.player2 h3')
     player2Choice.textContent = 'ROCK'
     p2 = 'ROCK'
+    runGame()
   }
   selectRockP2.addEventListener('click', clickRockP2)
 
@@ -44,6 +42,7 @@ function main() {
     const player2Choice = document.querySelector('.player2 h3')
     player2Choice.textContent = 'PAPER'
     p2 = 'PAPER'
+    runGame()
   }
   selectPaperP2.addEventListener('click', clickPaperP2)
   // handles if player2 selects scissor
@@ -52,31 +51,28 @@ function main() {
     const player2Choice = document.querySelector('.player2 h3')
     player2Choice.textContent = 'SCISSORS'
     p2 = 'SCISSORS'
+    runGame()
   }
   selectScissorsP2.addEventListener('click', clickScissorsP2)
-
   // set up logic to determine which player has the better hand
-  if (p1 === p2) {
-    window.alert('Draw!')
-  } else if (
-    (p1 === 'ROCK' && p2 === 'SCISSORS') ||
-    (p1 === 'SCISSORS' && p2 === 'PAPER') ||
-    (p1 === 'PAPER' && p2 === 'ROCK')
-  ) {
-    window.alert('Player 1 won!')
-  } else if (
-    (p1 === 'SCISSORS' && p2 === 'ROCK') ||
-    (p1 === 'PAPER' && p2 === 'SCISSORS') ||
-    (p1 === 'ROCK' && p2 === 'PAPER')
-  ) {
-    window.alert('Player 2 won!')
+  function runGame() {
+    if (p1 === p2) {
+      window.alert('Draw!')
+    } else if (
+      (p1 === 'ROCK' && p2 === 'SCISSORS') ||
+      (p1 === 'SCISSORS' && p2 === 'PAPER') ||
+      (p1 === 'PAPER' && p2 === 'ROCK')
+    ) {
+      window.alert('Player 1 won!')
+    } else {
+      window.alert('Player 2 won!')
+    }
   }
-
   // setup reset button
   function resetHands() {
-    player1Choice = document.querySelector('.player1 h3')
+    let player1Choice = document.querySelector('.player1 h3')
     player1Choice.textContent = 'Your move'
-    player2Choice = document.querySelector('.player2 h3')
+    let player2Choice = document.querySelector('.player2 h3')
     player2Choice.textContent = 'Your move'
   }
   document.querySelector('button').addEventListener('click', resetHands)
