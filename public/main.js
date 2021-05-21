15b6d9585cf394f526afe22adee2d68b311051f7
Karl Frick <karl.frick5@gmail.com>
2021-05-21 12:33:46 -0400

    Added Lizard and Spock options and logic

--- public/main.js
+++ public/main.js
@@ -25,6 +25,22 @@ function main() {
     p1 = 'SCISSORS'
   }
   selectScissorsP1.addEventListener('click', clickScissorsP1)
+  // handles if player1 selects lizard
+  const selectLizardP1 = document.querySelector('.player1 .lizard')
+  function clickLizardP1() {
+    const player1Choice = document.querySelector('.player1 h3')
+    player1Choice.textContent = 'LIZARD'
+    p1 = 'LIZARD'
+  }
+  selectLizardP1.addEventListener('click', clickLizardP1)
+  // handles if player1 selects spock
+  const selectSpockP1 = document.querySelector('.player1 .spock')
+  function clickSpockP1() {
+    const player1Choice = document.querySelector('.player1 h3')
+    player1Choice.textContent = 'SPOCK'
+    p1 = 'SPOCK'
+  }
+  selectSpockP1.addEventListener('click', clickSpockP1)
   // player2--------------------------------------------------------
   // handles if player2 selects rock
   const selectRockP2 = document.querySelector('.player2 .rock')
@@ -54,6 +70,27 @@ function main() {
     runGame()
   }
   selectScissorsP2.addEventListener('click', clickScissorsP2)
+
+  // handles if player2 selects lizard
+  const selectLizardP2 = document.querySelector('.player2 .lizard')
+  function clickLizardP2() {
+    const player2Choice = document.querySelector('.player2 h3')
+    player2Choice.textContent = 'LIZARD'
+    p2 = 'LIZARD'
+    runGame()
+  }
+  selectLizardP2.addEventListener('click', clickLizardP2)
+
+  // handles if player2 selects spock
+  const selectSpockP2 = document.querySelector('.player2 .spock')
+  function clickSpockP2() {
+    const player2Choice = document.querySelector('.player2 h3')
+    player2Choice.textContent = 'SPOCK'
+    p2 = 'SPOCK'
+    runGame()
+  }
+  selectSpockP2.addEventListener('click', clickSpockP2)
+
   // set up logic to determine which player has the better hand
   function runGame() {
     if (p1 === p2) {
@@ -61,7 +98,14 @@ function main() {
     } else if (
       (p1 === 'ROCK' && p2 === 'SCISSORS') ||
       (p1 === 'SCISSORS' && p2 === 'PAPER') ||
-      (p1 === 'PAPER' && p2 === 'ROCK')
+      (p1 === 'PAPER' && p2 === 'ROCK') ||
+      (p1 === 'ROCK' && p2 === 'LIZARD') ||
+      (p1 === 'LIZARD' && p2 === 'SPOCK') ||
+      (p1 === 'SPOCK' && p2 === 'SCISSORS') ||
+      (p1 === 'SCISSORS' && p2 === 'LIZARD') ||
+      (p1 === 'LIZARD' && p2 === 'PAPER') ||
+      (p1 === 'PAPER' && p2 === 'SPOCK') ||
+      (p1 === 'SPOCK' && p2 === 'ROCK')
     ) {
       window.alert('Player 1 won!')
     } else {
